6c47a7709372dbe5fce7aaf2f7509eb110cc53fd
toastsandwich <shreyasxcore2003@gmail.com>
2025-10-15 22:33:46 +0530

    Update create_initramfs.sh

--- create_initramfs.sh
+++ create_initramfs.sh
@@ -2,13 +2,70 @@ set -e
 set -x
 
 LDD_ROOT=/home/toastsandwich/ldd
-INITRAMFS_DIR=$LDD_ROOT/initramfs
-mkdir $INITRAMFS_DIR
+INITRAMFS_DIR=$LDD_ROOT/sandbox
+mkdir -p $INITRAMFS_DIR
 
 mkdir -p $INITRAMFS_DIR/{bin,dev,etc,lib,lib64,mnt,proc,root,sbin,sys,tmp}
 chmod 1777 $INITRAMFS_DIR/tmp
 
-sudo cp -a /dev/{null, console, tty, ttyS0} $INITRAMFS_DIR/dev/
+sudo cp -a /dev/{null,console,tty,ttyS0} $INITRAMFS_DIR/dev/
 
 sudo busybox --install $INITRAMFS_DIR/bin
 sudo busybox --install $INITRAMFS_DIR/sbin
+
+sudo cat << EOF > $INITRAMFS_DIR/init
+#!bin/busybox sh
+
+# Mount /proc and /sys fs
+mount -t proc none /proc
+mount -t sysfs none /sys
+
+# Boot things
+
+# NIC up
+ip link set eth0 up
+ip addr add 10.0.2.15/24 dev eth0
+
+ip link set wlan0 up
+ip addr add 10.0.2.14/24 dev wlan0
+
+ip link set lo up
+
+# Wait for NIC
+sleep 0.5
+
+# mount driver-dir to guest
+mount -t nfs -o nolock host:/home/toastsandwich/ldd/driver-dir /mnt
+
+# Make the new shell as a login shell with -l option
+# Only login shell read /etc/profile
+setsid sh -c 'exec sh -l </dev/ttyS0 >/dev/ttyS0 2>&1'
+poweroff
+EOF
+
+chmod +x $INITRAMFS_DIR/init
+
+# name resolve
+sudo cat << EOF > $INITRAMFS_DIR/etc/hosts
+127.0.0.1    localhost
+10.0.2.2     host
+EOF
+
+# common alias
+sudo cat << EOF > $INITRAMFS_DIR/etc/profile
+alias ll='ls -l'
+EOF
+
+# busybox saves password in /etc/passwd directly, no /etc/shadow is needed.
+sudo cat << EOF > $INITRAMFS_DIR/etc/passwd
+root:x:0:0:root:/root:/bin/bash
+EOF
+
+# group file
+sudo cat << EOF > $INITRAMFS_DIR/etc/group
+root:x:0:
+EOF
+
+# build initramfs image
+cd $INITRAMFS_DIR
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
